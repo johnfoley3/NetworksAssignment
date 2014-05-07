5982fec2596b8c7623df27669b3d31ce8abc9feb
John Foley <johnfoley3@gmail.com>
2014-05-07 16:20:29 -0500

    y is a vowel

--- PiglatinTranslator.java
+++ PiglatinTranslator.java
@@ -74,7 +74,7 @@ public class PiglatinTranslator {
     for ( int i = 0; i < word.length(); i++ )
       if ( word.charAt(i)=='a' || word.charAt(i)=='e' ||
           word.charAt(i)=='i' || word.charAt(i)=='o' ||
-          word.charAt(i)=='u' )
+          word.charAt(i)=='u' || word.charAt(i)=='y' )
         return i;
     return 0;
   }
